2e20d75e66bd9316f541329420c13967dfde7ae2
John Paul Quintana <jpquintana2024@gmail.com>
2024-05-21 01:28:04 +0800

    Update record.blade.php

--- resources/views/user/record.blade.php
+++ resources/views/user/record.blade.php
@@ -21,7 +21,7 @@
                     {{-- record table here --}}
                     
                     <div class="overflow-x-auto shadow-md sm:rounded-lg px-2">
-                        <div class="flex items-center justify-between flex-column md:flex-row flex-wrap space-y-4 md:space-y-0 py-4 bg-white dark:bg-gray-900">
+                        <div class="flex items-center justify-end flex-column md:flex-row flex-wrap space-y-4 md:space-y-0 py-4 bg-white dark:bg-gray-900">
                             <div>
                                 <button id="dropdownActionButton" data-dropdown-toggle="dropdownAction" class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
                                     <span class="sr-only">Action button</span>
@@ -33,32 +33,26 @@
                                 <!-- Dropdown menu -->
                                 <div id="dropdownAction" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                                     <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownActionButton">
-                                        <li>
-                                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">All</a>
+                                        <li class="filter" data-name="all">
+                                            <a href="#" id="filter-all" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Display All</a>
                                         </li>
-                                        <li>
-                                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Waiting</a>
+                                        @foreach ($records as $f)
+                                        <li class="flex items-center gap-1 px-2 hover:bg-slate-100 filter" data-name="{{ $f->fname }}">
+                                            <img class="w-6 h-6 rounded-md" src="{{ asset('storage').'/'.$f->profile }}" alt="">
+                                            <a href="#" class="block px-4 py-2">{{ $f->fname }}</a>
                                         </li>
-                                        <li>
-                                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Recieving</a>
-                                        </li>
-                                        <li class="py-1">
-                                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Completed</a>
-                                        </li>
-                                        <li class="py-1">
-                                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Rejected</a>
-                                        </li>
+                                        @endforeach
                                     </ul>
                                     
                                 </div>
                             </div>
-                            <label for="table-search" class="sr-only">Search</label>
+                            {{-- <label for="table-search" class="sr-only">Search</label>
                             <div class="">
                                
                                 <input type="text" id="table-search-users" class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search here...">
-                            </div>
+                            </div> --}}
                         </div>
-                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <table id="records-table" class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
                                     <th scope="col" class="p-4">
@@ -91,7 +85,7 @@
                                 {{-- {{ $records }} --}}
                                 @foreach ($records as $record)
                                 {{-- {{ $record }} --}}
-                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    <tr data-name="{{ $record->fname }}" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                         <td class="w-4 p-4">
                                             <div class="flex items-center">
                                                 <input value="{{ $record->id }}" type="checkbox" class="checkbox-id w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
@@ -178,6 +172,24 @@
     @section('scripts')
         <script>
             $(document).ready(function() {
+
+                $('.filter').click(function(){
+                    // alert($(this).data('name'))
+                    filterTable($(this).data('name'))
+               }) 
+                
+
+               function filterTable(filter) {
+                    $('#records-table tbody tr').each(function() {
+                        const rowName = $(this).data('name');
+                        if (filter === 'all' || rowName === filter) {
+                            $(this).show();
+                        } else {
+                            $(this).hide();
+                        }
+                    });
+                }
+                
 
                 let {
                     title,
