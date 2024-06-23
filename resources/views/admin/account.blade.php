2b1e80a4e2cd8ee218995eeb06f095e76f1354d7
John Paul Quintana <jpquintana2024@gmail.com>
2024-06-24 03:20:48 +0800

    Update account.blade.php

--- resources/views/admin/account.blade.php
+++ resources/views/admin/account.blade.php
@@ -86,6 +86,9 @@
                                             @if ($v->verified !== 1)
                                                 <button type="button" data-id="{{ $v->id }}" class="verifiedBtn bg-blue-500 hover:bg-blue-700 p-1 text-white rounded-md"><i class="fa-solid fa-badge-check"></i> verify</button>
                                                 <button type="button" data-id="{{ $v->id }}" class="rejectBtn bg-red-500 hover:bg-red-700 p-1 text-white rounded-md"><i class="fa-solid fa-xmark-to-slot"></i> reject</button>
+
+                                            @else
+                                            <button type="button" data-id="{{ $v->id }}" class="viewBtn bg-blue-500 hover:bg-blue-700 p-1 text-white rounded-md"><i class="fa-solid fa-badge-check"></i> view</button>
                                             @endif
                                             
                                         </td>
